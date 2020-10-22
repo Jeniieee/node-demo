2bbb2b31a6cdd2e424d92280e121a824533d92a9
jingee <30823119+Jeniieee@users.noreply.github.com>
2020-10-22 17:40:02 +0800

    托管静态资源

--- src/index.js
+++ src/index.js
@@ -11,11 +11,44 @@
 // console.log(d.num);
 
 var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+
+function notFound(req, res) {
+  fs.readFile(path.join(__dirname, "404.html"), (err, data) => {
+    if (err) {
+      res.write(404, "Not Found");
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html;charset=utf-8" });
+      res.write(data);
+      res.end();
+    }
+  });
+}
 
 // create a server object:
 http
   .createServer(function (req, res) {
-    res.write("Hello nodejs!"); //write a response to the client
-    res.end(); //end the response
+    let pathName = url.parse(req.url).pathname;
+    console.log(pathName);
+    if (pathName === "/") {
+      pathName = path.join(__dirname, "index.html");
+    }
+
+    const extName = path.extname(pathName);
+    if (extName === ".html") {
+      fs.readFile(pathName, (err, data) => {
+        if (err) {
+          notFound(req, res);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html;charset='utf-8'" });
+          res.write(data);
+          res.end();
+        }
+      });
+    }
+    // res.write("Hello nodejs!"); //write a response to the client
+    // res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
